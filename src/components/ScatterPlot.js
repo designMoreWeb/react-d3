e1820de7c31bde06a823ced1277500a958451a6e
designMoreWeb <divystolia@gmail.com>
2019-02-01 14:21:26 -0500

    create scatter plot circles

--- src/components/ScatterPlot.js
+++ src/components/ScatterPlot.js
@@ -3,3 +3,13 @@ import * as d3 from "d3";
 import styled from "styled-components";
 
 import { tooltipContext } from "./ToolTips";
+
+const Circle = styled.circle`
+    fill:yellow;
+    fill-opacity: 0.2;
+    stroke-width:1px;
+    cursor:pointer;
+    &:hover{
+        fill:red;
+    }
+`;
