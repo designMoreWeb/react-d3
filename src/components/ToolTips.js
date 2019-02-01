eb895b37ce488a1cca20cbb2ba938ed9a9ae6be8
designMoreWeb <divystolia@gmail.com>
2019-02-01 14:15:38 -0500

    completed tooltip

--- src/components/ToolTips.js
+++ src/components/ToolTips.js
@@ -1,1 +1,11 @@
 import React, { useState } from "react";
+
+const tooltipContext = React.createContext();
+
+function Tooltip (){
+    const [Tooltip,setTooltip] = useState(false);
+
+    return{ Tooltip,setTooltip };
+}
+
+export { Tooltip,tooltipContext };
