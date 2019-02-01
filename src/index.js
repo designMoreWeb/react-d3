f4a734d24b6983aa39bb0e2a500d5a918bb942b5
designMoreWeb <divystolia@gmail.com>
2019-02-01 10:42:33 -0500

    Added Components folder Added D3, Faker using npm Added imports to index.js created index.css, Scatterplot.js, tooltips.js removed uneeded files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React,{ useMemo, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Faker from 'faker';
+import { range } from "d3-array";
+import { scaleOrdinal } from "d3-scale";
+//User based component
+import ScatterPlot from 'ScatterPlot';
+import { Tooltip, tooltipContext } from "./ToolTip";
+import "./index.css";
